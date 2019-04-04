af4655a887765163f22aa9e85719230d6922ca00
Rodpoolazo <rodpoolazo@gmail.com>
2019-04-03 21:58:59 -0300

    add terminado (creo)

--- mayor_de_4.rb
+++ mayor_de_4.rb
@@ -6,17 +6,30 @@ b = ARGV[1].to_i
 c = ARGV[2].to_i
 d = ARGV[3].to_i
 
-if a>b && a>c && a>d
+
+
+if a>b and a>c and a>d
   puts "#{a}"
 
-elsif b>a && b>c && b>d
+elsif b>a and b>c and b>d
   puts "#{b}"
 
-elsif c>a && c>b && c>d
+elsif c>a and c>b and c>d
   puts "#{c}"
 
-elsif d>a && d>b && d>c
+elsif d>a and d>b and d>c
   puts "#{d}"
-else
-  puts "#{a}"
+end
+
+
+if d = nil
+  if a>b and a>c
+    puts "#{a}"
+
+  elsif b>a and b>c
+    puts "#{b}"
+
+  elsif c>a and c>b
+    puts "#{c}"
+  end
 end
